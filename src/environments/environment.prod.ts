64a26a4c580e46b2064c4eab772ef8c7e447fa3a
Brandon Kappel <brandon@fischertech.solutions>
2022-08-12 18:20:28 -0500

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   // apiUrl: "http://progressionalfitness-env-1.eba-8jtqsveg.us-east-2.elasticbeanstalk.com/api"
   apiUrl: "https://api.progressionalfitness.com/api"
 };
